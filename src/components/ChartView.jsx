77fce61f384eda324ec42df7c7c648197aab039d
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-09-10 21:13:59 +0100

    Add config panel to chart view

--- src/components/ChartView.jsx
+++ src/components/ChartView.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Chart from 'components/Chart';
+
+const StyledChartView = styled.div`
+    display: flex;
+    height: 100%;
+`;
+
+const ChartConfigPanel = styled.div`
+    background-color: green;
+    flex: 0 1 auto;
+`;
+
+const ChartPanel = styled.div`
+    flex: 1 1 auto;
+`;
 
 function ChartView(props) {
     const { dataFrame } = props;
@@ -8,7 +23,14 @@ function ChartView(props) {
     const xColumn = dataFrame.getColumn('Time');
     const yColumn = dataFrame.getColumn('Random number');
 
-    return <Chart xColumn={xColumn} yColumn={yColumn} />
+    return <StyledChartView>
+        <ChartConfigPanel>
+            Hello, world!
+        </ChartConfigPanel>
+        <ChartPanel>
+            <Chart xColumn={xColumn} yColumn={yColumn} />
+        </ChartPanel>
+    </StyledChartView>
 }
 
 export default ChartView;
